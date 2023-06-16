493f24ed73874378c32dbf84cf4364eb432c415d
Florin Alexandru Sirbu <florinsirbu280599@gmail.com>
2023-06-16 08:35:16 +0200

    ok

--- SinkshipJS/index.js
+++ SinkshipJS/index.js
@@ -1,22 +1,26 @@
 window.addEventListener("load", () => {
   SinkShip.init();
-  SinkShip.ShipHandler();
+  SinkShip.shipHandler();
+  SinkShip.buildInventory();
 });
 
 const limiter = document.createElement("div");
 limiter.classList.add("limiter");
 
-const playerFieldArray = [];
-const computerFieldArray = [];
-
 let SinkShip = {
   playerField: [],
   ComputerField: [],
+  inventory: [],
+  selectedShipType: "",
+  selectedShipOrientation: "",
+  selectedShipSize: "",
+
   init() {
     document.body.appendChild(this.makeHeader());
     document.body.appendChild(this.makeMain());
     document.body.appendChild(this.makeFooter());
-    this.ShipHandler();
+    this.shipHandler();
+    this.buildInventory();
   },
   makeHeader() {
     const header = document.createElement("header");
@@ -32,7 +36,7 @@ let SinkShip = {
     const controls = this.makeControls();
     const fields = this.makeDiv("fields");
     const playerField = this.makeField();
-    const computerField = this.BuildMenu();
+    const computerField = this.buildMenu();
 
     playerField.id = "playerfield";
     computerField.id = "computerfield";
@@ -86,7 +90,7 @@ let SinkShip = {
     button.innerHTML = label;
     return button;
   },
-  BuildMenu() {
+  buildMenu() {
     const computerField = this.makeDiv("field");
     computerField.classList.add("field");
     const table = document.createElement("table");
@@ -160,13 +164,15 @@ let SinkShip = {
 
   makeTableData(data) {
     const td = document.createElement("td");
+
     console.log(data.length);
     console.log(data);
     if (data.length == 3) {
       td.className = Array.isArray(data) ? data.join(" ") : data;
       td.addEventListener("click", () => {
         console.log("Clicked:", data);
-        this.showUsablePositions(data, this.playerField); // Pass playerField array
+        this.showUsablePositions(data, this.playerField);
+        this.getNextFreeShip(data);
       });
     } else {
       td.innerHTML = data;
@@ -197,37 +203,208 @@ let SinkShip = {
   },
 
   showAvaliablePosition(x, y, size, playerField, data) {
-    if (data[1] === "h") {
-      if (x < size) {
-        playerField[y][x].style.backgroundColor = "green";
-        playerField[y][x].className = "cell usable";
-      } else {
-        playerField[y][x].style.backgroundColor = "grey";
-        playerField[y][x].className = "cell disabled";
-      }
-    } else {
-      if (y < size) {
-        playerField[y][x].style.backgroundColor = "green";
-        playerField[y][x].className = "cell usable";
-      } else {
-        playerField[y][x].style.backgroundColor = "grey";
-        playerField[y][x].className = "cell disabled";
+    if (
+      !playerField[y][x].classList.contains("horizontal") &&
+      !playerField[y][x].classList.contains("vertical") &&
+      !playerField[y][x].classList.contains("left") &&
+      !playerField[y][x].classList.contains("right") &&
+      !playerField[y][x].classList.contains("top") &&
+      !playerField[y][x].classList.contains("bottom")
+    ) {
+      if (data[1] === "h") {
+        if (x < size) {
+          playerField[y][x].style.backgroundColor = "green";
+          playerField[y][x].className = "cell usable";
+        } else {
+          playerField[y][x].style.backgroundColor = "grey";
+          playerField[y][x].className = "cell disabled";
+        }
+      } else if (data[1] === "v") {
+        if (y < size) {
+          playerField[y][x].style.backgroundColor = "green";
+          playerField[y][x].className = "cell usable";
+        } else {
+          playerField[y][x].style.backgroundColor = "grey";
+          playerField[y][x].className = "cell disabled";
+        }
       }
     }
   },
 
-  ShipHandler() {
-    for (let y = 0; y < 10; y++) {
-      for (let x = 0; x < 10; x++) {
+  shipHandler() {
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 10; y++) {
         const cell = this.playerField[y][x];
         cell.addEventListener("click", () => {
           if (cell.classList.contains("usable")) {
-            console.log(x, y);
+            console.log(`Ship start: X: ${x}, Y: ${y}`);
+            this.drawShip(x, y);
+            console.log(
+              `Orientation: ${this.selectedShipOrientation}, Size: ${this.selectedShipSize}, Type: ${this.selectedShipType}`
+            );
+
+            cell.classList.remove("usable");
+            if (this.selectedShipOrientation === "w") {
+              for (let i = 0; i < this.selectedShipSize; i++) {
+                const currentCell = this.playerField[y][x + i];
+                currentCell.classList.add(
+                  i === 0
+                    ? "left"
+                    : i === this.selectedShipSize - 1
+                    ? "right"
+                    : "horizontal"
+                );
+              }
+            } else if (this.selectedShipOrientation === "h") {
+              for (let i = 0; i < this.selectedShipSize; i++) {
+                const currentCell = this.playerField[y + i][x];
+                currentCell.classList.add(
+                  i === 0
+                    ? "top"
+                    : i === this.selectedShipSize - 1
+                    ? "bottom"
+                    : "vertical"
+                );
+              }
+            }
+
+            if (!this.playButtonPressed) {
+              const removedShipType = this.selectedShipType;
+              const shipCells = [];
+              if (this.selectedShipOrientation === "w") {
+                for (let i = 0; i < this.selectedShipSize; i++) {
+                  const shipCell = this.playerField[y][x + i];
+                  shipCells.push(shipCell);
+                }
+              } else if (this.selectedShipOrientation === "h") {
+                for (let i = 0; i < this.selectedShipSize; i++) {
+                  const shipCell = this.playerField[y + i][x];
+                  shipCells.push(shipCell);
+                }
+              }
+
+              shipCells.forEach((shipCell) => {
+                shipCell.addEventListener("click", () => {
+                  if (!this.isShipPlaced(shipCells)) {
+                    return;
+                  }
+                  const clickedIndex = shipCells.indexOf(shipCell);
+
+                  shipCells.forEach((cell) => {
+                    cell.classList.remove(
+                      "left",
+                      "right",
+                      "top",
+                      "bottom",
+                      "horizontal",
+                      "vertical"
+                    );
+                  });
+
+                  shipCell.removeEventListener("click", () => {});
+
+                  const removedShip = this.inventory.find(
+                    (ship) => ship.type === removedShipType
+                  );
+                  if (removedShip) {
+                    removedShip.available++;
+
+                    if (removedShip.available === 2) {
+                      const tableRows = document.querySelectorAll("tbody tr");
+                      tableRows.forEach((row) => {
+                        const typeDiv = row.querySelector("td div");
+                        if (typeDiv.textContent === removedShipType) {
+                          const cells = row.querySelectorAll(".w, .h");
+                          cells.forEach((cell) => {
+                            cell.classList.remove("off");
+                          });
+                        }
+                      });
+                    }
+                  }
+                });
+              });
+            }
+            this.resetCells();
           }
         });
       }
     }
   },
-};
 
-//Inventory
+  resetCells() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      cell.classList.remove("usable", "disable");
+      cell.classList.remove("w", "h");
+      cell.removeEventListener("click", this.cellClickHandler);
+    });
+  },
+
+  buildInventory() {
+    this.inventory = [
+      { type: "battleship", size: "5", available: 1 },
+      { type: "cruiser", size: "4", available: 2 },
+      { type: "destroyer", size: "3", available: 3 },
+      { type: "submarine", size: "2", available: 4 },
+    ];
+  },
+
+  getNextFreeShip(data) {
+    this.saveShipData(data);
+    for (let i = 0; i < this.inventory.length; i++) {
+      if (data[0] === this.inventory[i].type) {
+        if (this.inventory[i].available <= 0) {
+          this.hideShip(data);
+          return 0;
+        } else {
+          console.log(data[0]);
+          this.inventory[i].available -= 1;
+          console.log(this.inventory[i].available);
+        }
+      }
+    }
+  },
+
+  saveShipData(data) {
+    this.selectedShipType = data[0];
+    for (let i = 0; i < this.inventory.length; i++) {
+      if (data[0] === this.inventory[i].type) {
+        this.selectedShipSize = this.inventory[i].size;
+        data[1] === "h"
+          ? (this.selectedShipOrientation = "horizontal")
+          : (this.selectedShipOrientation = "vertical");
+      }
+    }
+  },
+
+  drawShip(x, y) {
+    console.log(this.selectedShipSize);
+    if (this.selectedShipOrientation === "horizontal") {
+      const firstCell = this.playerField[y][x];
+      firstCell.classList.add("cell", "left");
+
+      for (let i = 1; i < this.selectedShipSize; i++) {
+        const cell = this.playerField[y][x + i];
+        cell.classList.add("cell", "horizontal");
+
+        if (i === this.selectedShipSize - 1) {
+          cell.classList.add("right");
+        }
+      }
+    } else if (this.selectedShipOrientation === "vertical") {
+      const firstCell = this.playerField[y][x];
+      firstCell.classList.add("cell", "top");
+
+      for (let i = 1; i < this.selectedShipSize; i++) {
+        const cell = this.playerField[y + i][x];
+        cell.classList.add("cell", "vertical");
+
+        if (i === this.selectedShipSize - 1) {
+          cell.classList.remove("vertical");
+          cell.classList.add("bottom");
+        }
+      }
+    }
+  },
+};
